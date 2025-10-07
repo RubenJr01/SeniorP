9bdd492e6733f315b41ab7e29e9d3c7ceb60f916
Ruben de la Cruz Jr <ruben.delacruzjr01@gmail.com>
2025-10-06 23:03:04 -0500

    Front/Back end connection active

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,40 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import "./styles/Form.css";
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout() {
+  localStorage.clear(); // Make sure to clear old/recent tokens to make sure no bugs occur
+  return <Register />;
+}
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,27 +2,50 @@ import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Custom front-end protection
 function ProtectedRoute({ children }) {
   const [isAuthorized, setIsAuthorized] = useState(null);
 
-  const refreshToken = async () => {};
+  useEffect(() => {
+    auth().catch(() => setIsAuthorized(false));
+  }, []);
+
+  // Getting refresh token, TRY to send response to root, with token, which should give back another access token
+  const refreshToken = async () => {
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+    try {
+      const resp = await api.post("/api/token/refresh/", {
+        refresh: refreshToken,
+      });
+      if (resp.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, resp.data.access);
+        setIsAuthorized(true);
+      } else {
+        setIsAuthorized(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsAuthorized(false);
+    }
+  };
 
   // Need to check if token is valid/expired
   const auth = async () => {
-    const token = localStorage.getItem(ACCESS_TOKEN)
-    if(!token) {
-      setIsAuthorized(false)
-      return
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!token) {
+      setIsAuthorized(false);
+      return;
     }
-    const decoded = jwtDecode(token)
-    const tokenExpiration = decoded.exp
-    const now = Date.now() / 1000
+    const decoded = jwtDecode(token);
+    const tokenExpiration = decoded.exp;
+    const now = Date.now() / 1000;
 
-    if(tokenExpiration < now) {
-      await refreshToken
+    if (tokenExpiration < now) {
+      await refreshToken();
+    } else {
+      setIsAuthorized(true);
     }
   };
 
