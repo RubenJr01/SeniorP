a4da67e5331d7280da7f29f7f1ff1784fdf05e32
Ruben de la Cruz Jr <ruben.delacruzjr01@gmail.com>
2025-10-08 20:22:01 -0500

    Allowing cloudflared tunnel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// cloudflared tunnel for toDateString();
+const TUNNEL_HOST = 'https://eva-cayman-wright-watt.trycloudflare.com'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 0.0.0.0,
+    port: 5173,
+    allowedHosts: [TUNNEL_HOST],
+
+    // Help HMR work over HTTPS tunnel
+    hmr: {
+      host: TUNNEL_HOST,
+      protocol: 'wss',
+      clientPort: 443,
+    },
+  },
 })
